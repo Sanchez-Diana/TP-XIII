8682a9e7e550e46f85bc66c3504ce5c3e9f7d5ef
Sanchez-Diana <sanchez.diana@tecnica7.edu.ar>
2024-12-11 17:47:39 -0300

    commit

---
+++ public/js/eliminar.js
@@ -0,0 +1,49 @@
+mostrarMensaje = (mensaje) => {
+  document.querySelector('#mensajeLog').innerHTML = mensaje;
+}
+
+mostrarMensaje = (mensaje) => {
+  console.log(mensaje);
+  console.log(document.querySelector('#mensajeBack'));
+  document.querySelector('#mensajeBack').className += " bg-warning";
+  document.querySelector('#mensajeBack').innerHTML = mensaje;
+}
+
+document.getElementById('eliminar').addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const contraseña = document.getElementById('contraseña').value;
+
+  if (!contraseña) {
+      console.log('faltan datos');
+      document.querySelector('#mensajeLog').innerHTML = '*Complete todos los datos';
+      return;
+  }
+
+  try {
+      const enviarDatos = await fetch('/eliminar', {
+          method: 'post',
+          headers: {
+              'content-type': 'application/json'
+          },
+          body: JSON.stringify({
+              contraseña
+          })
+      });
+
+      const respuesta = await enviarDatos.json();
+      console.log(respuesta);
+      mostrarMensaje(respuesta.mensaje);
+
+      if (enviarDatos.ok) {
+          localStorage.removeItem('usuarioLogueado');
+          setTimeout(() => {
+              window.location.href = '/index.html';
+          }, 2000); 
+
+      }
+  } catch (error) {
+      console.log(error);
+      mostrarMensaje('Error al borrar cuenta');
+  }
+});
